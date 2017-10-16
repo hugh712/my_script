555ded760babf8b71ff0d960d8bacb27f6e2fd78
hugh712 <hugh712@gmail.com>
2017-10-16 22:20:28 +0800

    update my_game.py

--- my_game.py
+++ my_game.py
@@ -23,9 +23,14 @@ while go:
 	
 	# restart 
 	# 681, 787
-	print "restart"
-	device.touch(600, 1225 , MonkeyDevice.DOWN)
-        device.touch(600, 1225 , MonkeyDevice.UP)
+	print "left coner"
+	device.touch(65, 1220 , MonkeyDevice.DOWN)
+        device.touch(65, 1220 , MonkeyDevice.UP)
+	sleep(5)
+        
+        print "stage 1"
+	device.touch(530, 1015 , MonkeyDevice.DOWN)
+        device.touch(530, 1015 , MonkeyDevice.UP)
 	sleep(5)
 
         # next turn
